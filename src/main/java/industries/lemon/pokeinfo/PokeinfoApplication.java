c718ea656451267a20cc6b5be8e8f58272f2bb47
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-09-01 13:43:08 +0200

    initial commit

---
+++ src/main/java/industries/lemon/pokeinfo/PokeinfoApplication.java
@@ -0,0 +1,13 @@
+package industries.lemon.pokeinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeinfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/industries/lemon/pokeinfo/PokeinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package industries.lemon.pokeinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
